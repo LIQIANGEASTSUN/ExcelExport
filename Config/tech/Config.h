8605a1d386c3bd8ebad02d63251fb467ad2a0983
liqiang <liqiangeastsun@163.com>
2024-03-19 14:58:23 +0800

    modify

--- Config/tech/Config.h
+++ Config/tech/Config.h
@@ -69,4 +69,202 @@ struct table_text_localizationT : ConfigBaseT
 };
 CreateCsvClass(table_text_localization);
 
+struct Terrain1T : ConfigBaseT
+{
+	virtual ~Terrain1T()
+	{
+	}
+	Terrain1T()
+	{
+		FileName = "Terrain1.csv";
+		_kf =
+		{
+			{"Terraiin",{INT_FD, &Terraiin}},
+			{"ColCount",{INT_FD, &ColCount}},
+			{"col0",{TSTR_FD, &col0}},
+			{"col1",{TSTR_FD, &col1}},
+			{"col2",{TSTR_FD, &col2}},
+			{"col3",{TSTR_FD, &col3}},
+			{"col4",{TSTR_FD, &col4}},
+			{"col5",{TSTR_FD, &col5}},
+			{"col6",{TSTR_FD, &col6}},
+			{"col7",{TSTR_FD, &col7}},
+			{"col8",{TSTR_FD, &col8}},
+			{"col9",{TSTR_FD, &col9}},
+			{"col10",{TSTR_FD, &col10}},
+			{"col11",{TSTR_FD, &col11}},
+			{"col12",{TSTR_FD, &col12}},
+			{"col13",{TSTR_FD, &col13}},
+			{"col14",{TSTR_FD, &col14}},
+			{"col15",{TSTR_FD, &col15}},
+			{"col16",{TSTR_FD, &col16}},
+			{"col17",{TSTR_FD, &col17}},
+			{"col18",{TSTR_FD, &col18}},
+			{"col19",{TSTR_FD, &col19}}
+		};
+	}
+	virtual Terrain1T* create()
+	{
+		return new Terrain1T();
+	}
+	enum
+	{
+	};
+	INT32 Terraiin = 0;            //行ID
+	INT32 ColCount = 0;				//列总数
+	TStr col0 = "";				//行数据
+	TStr col1 = "";				//行数据
+	TStr col2 = "";				//行数据
+	TStr col3 = "";				//行数据
+	TStr col4 = "";				//行数据
+	TStr col5 = "";				//行数据
+	TStr col6 = "";				//行数据
+	TStr col7 = "";				//行数据
+	TStr col8 = "";				//行数据
+	TStr col9 = "";				//行数据
+	TStr col10 = "";				//行数据
+	TStr col11 = "";				//行数据
+	TStr col12 = "";				//行数据
+	TStr col13 = "";				//行数据
+	TStr col14 = "";				//行数据
+	TStr col15 = "";				//行数据
+	TStr col16 = "";				//行数据
+	TStr col17 = "";				//行数据
+	TStr col18 = "";				//行数据
+	TStr col19 = "";				//行数据
+};
+CreateCsvClass(Terrain1);
+
+struct Terrain6T : ConfigBaseT
+{
+	virtual ~Terrain6T()
+	{
+	}
+	Terrain6T()
+	{
+		FileName = "Terrain6.csv";
+		_kf =
+		{
+			{"Terraiin",{INT_FD, &Terraiin}},
+			{"ColCount",{INT_FD, &ColCount}},
+			{"col0",{TSTR_FD, &col0}},
+			{"col1",{TSTR_FD, &col1}},
+			{"col2",{TSTR_FD, &col2}},
+			{"col3",{TSTR_FD, &col3}},
+			{"col4",{TSTR_FD, &col4}},
+			{"col5",{TSTR_FD, &col5}},
+			{"col6",{TSTR_FD, &col6}},
+			{"col7",{TSTR_FD, &col7}},
+			{"col8",{TSTR_FD, &col8}},
+			{"col9",{TSTR_FD, &col9}},
+			{"col10",{TSTR_FD, &col10}},
+			{"col11",{TSTR_FD, &col11}},
+			{"col12",{TSTR_FD, &col12}},
+			{"col13",{TSTR_FD, &col13}},
+			{"col14",{TSTR_FD, &col14}},
+			{"col15",{TSTR_FD, &col15}},
+			{"col16",{TSTR_FD, &col16}},
+			{"col17",{TSTR_FD, &col17}},
+			{"col18",{TSTR_FD, &col18}},
+			{"col19",{TSTR_FD, &col19}}
+		};
+	}
+	virtual Terrain6T* create()
+	{
+		return new Terrain6T();
+	}
+	enum
+	{
+	};
+	INT32 Terraiin = 0;            //行ID
+	INT32 ColCount = 0;				//列总数
+	TStr col0 = "";				//行数据
+	TStr col1 = "";				//行数据
+	TStr col2 = "";				//行数据
+	TStr col3 = "";				//行数据
+	TStr col4 = "";				//行数据
+	TStr col5 = "";				//行数据
+	TStr col6 = "";				//行数据
+	TStr col7 = "";				//行数据
+	TStr col8 = "";				//行数据
+	TStr col9 = "";				//行数据
+	TStr col10 = "";				//行数据
+	TStr col11 = "";				//行数据
+	TStr col12 = "";				//行数据
+	TStr col13 = "";				//行数据
+	TStr col14 = "";				//行数据
+	TStr col15 = "";				//行数据
+	TStr col16 = "";				//行数据
+	TStr col17 = "";				//行数据
+	TStr col18 = "";				//行数据
+	TStr col19 = "";				//行数据
+};
+CreateCsvClass(Terrain6);
+
+struct Terrain7T : ConfigBaseT
+{
+	virtual ~Terrain7T()
+	{
+	}
+	Terrain7T()
+	{
+		FileName = "Terrain7.csv";
+		_kf =
+		{
+			{"Terraiin",{INT_FD, &Terraiin}},
+			{"ColCount",{INT_FD, &ColCount}},
+			{"col0",{TSTR_FD, &col0}},
+			{"col1",{TSTR_FD, &col1}},
+			{"col2",{TSTR_FD, &col2}},
+			{"col3",{TSTR_FD, &col3}},
+			{"col4",{TSTR_FD, &col4}},
+			{"col5",{TSTR_FD, &col5}},
+			{"col6",{TSTR_FD, &col6}},
+			{"col7",{TSTR_FD, &col7}},
+			{"col8",{TSTR_FD, &col8}},
+			{"col9",{TSTR_FD, &col9}},
+			{"col10",{TSTR_FD, &col10}},
+			{"col11",{TSTR_FD, &col11}},
+			{"col12",{TSTR_FD, &col12}},
+			{"col13",{TSTR_FD, &col13}},
+			{"col14",{TSTR_FD, &col14}},
+			{"col15",{TSTR_FD, &col15}},
+			{"col16",{TSTR_FD, &col16}},
+			{"col17",{TSTR_FD, &col17}},
+			{"col18",{TSTR_FD, &col18}},
+			{"col19",{TSTR_FD, &col19}}
+		};
+	}
+	virtual Terrain7T* create()
+	{
+		return new Terrain7T();
+	}
+	enum
+	{
+	};
+	INT32 Terraiin = 0;            //行ID
+	INT32 ColCount = 0;				//列总数
+	TStr col0 = "";				//行数据
+	TStr col1 = "";				//行数据
+	TStr col2 = "";				//行数据
+	TStr col3 = "";				//行数据
+	TStr col4 = "";				//行数据
+	TStr col5 = "";				//行数据
+	TStr col6 = "";				//行数据
+	TStr col7 = "";				//行数据
+	TStr col8 = "";				//行数据
+	TStr col9 = "";				//行数据
+	TStr col10 = "";				//行数据
+	TStr col11 = "";				//行数据
+	TStr col12 = "";				//行数据
+	TStr col13 = "";				//行数据
+	TStr col14 = "";				//行数据
+	TStr col15 = "";				//行数据
+	TStr col16 = "";				//行数据
+	TStr col17 = "";				//行数据
+	TStr col18 = "";				//行数据
+	TStr col19 = "";				//行数据
+};
+CreateCsvClass(Terrain7);
+
 #endif
