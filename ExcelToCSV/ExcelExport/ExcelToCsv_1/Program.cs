e4357cac6c53db0a86169b47c3c3ba7d478c236b
liqiang <liqiangeastsun@163.com>
2024-09-17 11:36:30 +0800

    整理代码

---
+++ ExcelToCSV/ExcelExport/ExcelToCsv_1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using ExcelExport;
+
+namespace ExcelToCsv_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("开始导出 CSV");
+
+            //string path = "G:\\Project\\Git\\ExcelToCSV\\New\\ExcelToCSV\\Table\\Builds222.xlsx";
+            string path = "G:\\Git\\ExcelToCSV\\ExcelToCSV\\Table\\Builds222.xlsx";
+
+            ReadExcel readExcel = new ReadExcel(path);
+            new WriteCsvClient(readExcel);
+            new WriteCsvServer(readExcel);
+
+            Console.WriteLine("导表结束");
+
+            Console.ReadLine();
+        }
+    }
+}
