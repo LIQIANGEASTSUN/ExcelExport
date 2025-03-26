ef16c8a7a1e051c7953faefaf757724c69b42676
liqiang <qiang.li.9631@gmail.com>
2025-03-26 17:22:41 +0800

    modify

--- ExcelExport/ExcelExport/ExcelExport/Write/CS/WriteJsonCsAnalysis.cs
+++ ExcelExport/ExcelExport/ExcelExport/Write/CS/WriteJsonCsAnalysis.cs
@@ -33,7 +33,10 @@ namespace ExcelExport
             sb.AppendLine("using System;");
             sb.AppendLine();
             sb.AppendLine($"public class JsonCsAnalysis {lb}");
+            sb.AppendLine();
+            sb.AppendLine($"    public const int TotalCount = {_list.Count}");
             sb.AppendLine($"    private Action OneLoadSuccess;");
+            sb.AppendLine();
             sb.AppendLine($"    public void SetLoadCallBack(Action oneLoadSuccess)");
             sb.AppendLine($"    {lb}");
             sb.AppendLine($"        OneLoadSuccess = oneLoadSuccess;");
